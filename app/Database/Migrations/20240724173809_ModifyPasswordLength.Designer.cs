e3f109aa086bbf3164b759d47c37934d3e8542f9
roney ruiz <roneyruizrojas@gmail.com>
2024-07-24 11:40:37 -0600

    modify password length

--- app/Database/Migrations/20240724173809_ModifyPasswordLength.Designer.cs
+++ app/Database/Migrations/20240724173809_ModifyPasswordLength.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Veterinarian_Dotnet_Api.App.Database;
@@ -11,9 +12,11 @@ using Veterinarian_Dotnet_Api.App.Database;
 namespace veterinarian_dotnet_api.app.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240724173809_ModifyPasswordLength")]
+    partial class ModifyPasswordLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- app/Models/User.cs
+++ app/Models/User.cs
@@ -14,7 +14,6 @@ public class User
 
   [Required]
   [MinLength(8)]
-  [MaxLength(20)]
   public required string Password { get; set; }
   
   public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
