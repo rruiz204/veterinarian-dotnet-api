ea81b5ba860ceb13f36842271df17e0a76f9bc53
roney ruiz <roneyruizrojas@gmail.com>
2024-07-24 09:39:23 -0600

    create the user model

---
+++ app/Models/User.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Veterinarian_Dotnet_Api.App.Models;
+
+public class User
+{
+  [Key]
+  public int Id { get; set; }
+
+  [Required]
+  [EmailAddress]
+  [MaxLength(150)]
+  public required string Email { get; set; }
+
+  [Required]
+  [MinLength(8)]
+  [MaxLength(20)]
+  public required string Password { get; set; }
+  
+  public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+  public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
