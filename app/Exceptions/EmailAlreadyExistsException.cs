434508fced11ec0d47151b244d23edb3dfeb1bc7
roney ruiz <roneyruizrojas@gmail.com>
2024-07-26 09:24:15 -0600

    modify the filter

--- app/Exceptions/EmailAlreadyExistsException.cs
+++
@@ -1,6 +0,0 @@
-namespace Veterinarian_Dotnet_Api.App.Exceptions;
-
-public class EmailAlreadyExistsException : Exception
-{
-  public EmailAlreadyExistsException() : base("This email already exists") {}
-}
--- app/Filters/ArgumentExceptionFilter.cs
+++ app/Filters/ArgumentExceptionFilter.cs
@@ -1,14 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Veterinarian_Dotnet_Api.App.Exceptions;
 
 namespace Veterinarian_Dotnet_Api.App.Filters;
 
-public class EmailAlreadyExistsExceptionFilter : IExceptionFilter
+public class ArgumentExceptionFilter : IExceptionFilter
 {
   public void OnException(ExceptionContext context)
   {
-    if (context.Exception is EmailAlreadyExistsException)
+    if (context.Exception is ArgumentException)
     {
       context.Result = new BadRequestObjectResult(context.Exception.Message);
       context.ExceptionHandled = true;
