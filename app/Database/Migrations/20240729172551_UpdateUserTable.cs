ffa3e3429160129490e86e34f48576c0f8938d47
roney ruiz <roneyruizrojas@gmail.com>
2024-07-29 14:08:44 -0600

    update user table

---
+++ app/Database/Migrations/20240729172551_UpdateUserTable.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace veterinarian_dotnet_api.app.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "users",
+                type: "character varying(9)",
+                maxLength: 9,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "users");
+        }
+    }
+}
--- app/Models/User.cs
+++ app/Models/User.cs
@@ -8,6 +8,14 @@ public class User
   public int Id { get; set; }
 
   [Required]
+  [StringLength(50)]
+  public required string FirstName { get; set; }
+
+  [Required]
+  [StringLength(50)]
+  public required string LastName { get; set; }
+
+  [Required]
   [EmailAddress]
   [MaxLength(150)]
   public required string Email { get; set; }
@@ -15,7 +23,12 @@ public class User
   [Required]
   [MinLength(8)]
   public required string Password { get; set; }
-  
+
+  [Required]
+  [Phone]
+  [StringLength(9)]
+  public required string PhoneNumber { get; set; }
+
   public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
   public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
