6e4e9ebf05b8837d1ad3c1cbb85f9a1b409c3513
roney ruiz <roneyruizrojas@gmail.com>
2024-08-07 23:06:29 -0600

    create role table

--- app/Database/DatabaseContext.cs
+++ app/Database/DatabaseContext.cs
@@ -6,11 +6,29 @@ namespace Veterinarian_Dotnet_Api.App.Database;
 public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
 {
     public DbSet<User> User { get; set; }
+    public DbSet<Role> Role { get; set; }
+    public DbSet<UserRole> UserRole { get; set; }
     public DbSet<ResetToken> ResetToken { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>().ToTable("users");
+        modelBuilder.Entity<Role>().ToTable("roles");
+        modelBuilder.Entity<UserRole>().ToTable("user_roles");
+
+        modelBuilder.Entity<UserRole>()
+           .HasKey(ur => new { ur.UserId, ur.RoleId });
+
+        modelBuilder.Entity<UserRole>()
+            .HasOne(ur => ur.User)
+            .WithMany(u => u.UserRoles)
+            .HasForeignKey(ur => ur.UserId);
+
+        modelBuilder.Entity<UserRole>()
+            .HasOne(ur => ur.Role)
+            .WithMany(r => r.UserRoles)
+            .HasForeignKey(ur => ur.RoleId);
+
         modelBuilder.Entity<ResetToken>().ToTable("reset_tokens");
     }
 }
